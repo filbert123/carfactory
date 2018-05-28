e9b0bb36e313255de978dba9cdad1ff4f6892a8d
Filbertinos <filbertinos@gmail.com>
2018-05-28 21:40:04 +0300

    fabric fix

--- src/main/java/Fabric.java
+++ src/main/java/Fabric.java
@@ -8,34 +8,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Fabric {
-    private String message;
     private Car car;
 
     public void setCar(Car car) {
         this.car = car;
     }
 
-    /*public void createCar(Car car){
-        this.car.create();
-    }*/
-
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
     public static void main(String[] args) {
-        createCars("Jetta_S",99);
+        createCars("Jetta_S",100);
 
     }
 
     public static void createCars(String id,int amount){
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
         List<Car> cars = new ArrayList<Car>();
-        for(int i=0;i<=amount;i++){
+        for(int i=0;i<amount;i++){
             Car car = (Car) context.getBean(id);
             cars.add(car);
+            //System.out.println(car);
         }
-        System.out.println("Fabric created "+cars.size()+" models ");
+        System.out.println("Fabric created "+cars.size()+" models "+id+" ");
+
     }
 }
